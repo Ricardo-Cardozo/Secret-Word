2b5353cb5ed41383fbc0338979de12393407102c
Ricardo Cardozo <ricardo_alexandrecardozo@hotmail.com>
2022-10-04 12:59:35 -0300

    Css da tela de jogo OK!

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,8 +3,29 @@ import './Game.css'
 const Game = ({verifyLetter}) => {
   return (
     <div className='game'>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar o Jogo</button>
+      <p className="points">
+        <span>Pontuuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button>Jogar!</button>
+        </form>
+        <div className="wrongLettersContainer">
+            <p>Letras já utilizadas</p>
+            <span>a, </span>
+            <span>b, </span>
+          </div>
+      </div>
     </div>
   )
 }
